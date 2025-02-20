847f56549202145d677ab7b17d5e2661a0208ca7
MahraibFatima <mahraibfatima5@gmail.com>
2025-02-20 19:27:14 +0500

    msg send functionality added

--- bot.py
+++ bot.py
@@ -1,1 +1,12 @@
-print("Hi, there's a reaction handler.")
+import slack
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+
+env_path= Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
+
+
+client = slack.WebClient(token=os.environ['SLACK_TOKEN_'])
+
+client.chat_postMessage(channel='#social', text="Hello World!")
