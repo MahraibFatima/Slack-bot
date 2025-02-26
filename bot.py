fd9fd3b769994f9e3362627ad5f81baba92169ce
MahraibFatima <mahraibfatima5@gmail.com>
2025-02-26 22:21:46 +0500

    welcome msg functionality

--- bot.py
+++ bot.py
@@ -15,9 +15,59 @@ client = slack.WebClient(token=os.environ['SLACK_TOKEN_'])
 BOT_ID = client.api_call("auth.test")['user_id']
 
 message_counts= {}
+welcome_messages = {}
 
-def send_message(message):
-    client.chat_postMessage(channel='#test', text=message)
+class WelcomeMessage:
+    START_TEXT = {
+        'type': 'section',
+        'text': {
+            'type': 'mrkdwn',
+            'text': (
+                'Welcome to this channel! \n\n'
+                '*Get started by completing the tasks!*'
+            )
+        }
+    }
+    DIVIDER = {'type': 'divider'}
+
+    def __init__(self, channel, user):
+        self.channel = channel
+        self.user = user
+        self.icon_emoji = ':robot_face:'
+        self.timestamp = ''
+        self.completed = False
+        #self.text = f"Welcome to the channel <@{user}>! We are glad to have you here!"
+    
+    def get_message_payload(self):
+        return {
+            'ts': self.timestamp,
+            'channel': self.channel,
+            'username': 'Welcome Robot!',
+            'icon_emoji': self.icon_emoji,
+            'blocks': [
+                self.START_TEXT,
+                self.DIVIDER,
+                *self._get_reaction_task()
+            ]
+        }
+    
+    def _t_reaction_task(self): 
+        checkmark = ':white_check_mark:'
+        if not self.completed:
+            checkmark = ':white_large_square:'
+        text = f"{checkmark} *React to this message!*"  
+        return [{'type': 'section', 'text': {'type': 'mrkdwn', 'text': text}}]
+
+def send_welcome_message(channel, user):
+    welcome = WelcomeMessage(channel, user)
+    message = welcome.get_message_payload()
+    response = client.chat_postMessage(**message) 
+    welcome.timestamp = response['ts']
+
+    if channel not in welcome_messages:
+        welcome_messages[channel] = {}
+    welcome_messages[channel][user] = welcome
+    #client.chat_postMessage(channel='#test', text=message)
 
 @slack_event_adapter.on('message')
 def message(payload):
@@ -25,12 +75,16 @@ def message(payload):
     channel_id = event.get('channel')
     user_id = event.get('user')
     text = event.get('text')
-    if BOT_ID != user_id:
+    if BOT_ID != user_id and user_id != None:
         if user_id in message_counts:
             message_counts[user_id] += 1
         else:
             message_counts[user_id] = 1
-        send_message(text)
+        if text.lower() == 'start':
+            send_welcome_message(f'@{user_id}', user_id)
+            # welcome = WelcomeMessage(channel_id, user_id)
+            # client.chat_postMessage(**welcome.get_message_payload())
+        #send_message(text)
 
 @app.route('/message-count', methods=['GET', 'POST'])
 def message_count():
