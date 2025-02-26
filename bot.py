742a810bb9390f72ee025048d8f3780352ad0798
MahraibFatima <mahraibfatima5@gmail.com>
2025-02-26 19:27:12 +0500

    bot messaged integration

--- bot.py
+++ bot.py
@@ -2,15 +2,31 @@ import slack
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+from flask import Flask
+from slackeventsapi import SlackEventAdapter
 
 env_path= Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
 
+app = Flask(__name__) 
+slack_event_adapter = SlackEventAdapter(os.environ['SIGNING_SECRET_'], '/slack/events', app)
+
 client = slack.WebClient(token=os.environ['SLACK_TOKEN_'])
+BOT_ID = client.api_call("auth.test")['user_id']
 
 def send_message(message):
     client.chat_postMessage(channel='#test', text=message)
 
+@slack_event_adapter.on('message')
+def message(payload):
+    event = payload.get('event', {})
+    channel_id = event.get('channel')
+    user_id = event.get('user')
+    text = event.get('text')
+    if BOT_ID != user_id:
+        send_message(text)
 
 if __name__ == "__main__": 
+    app.run(debug=True)
     send_message('Hello World!')
+    
